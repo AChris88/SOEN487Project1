b2d4927a0511cc40c2a467c4fbbd47b366aad355
GeorgeLambadas <g.lambadas@gmail.com>
2016-03-31 17:12:52 -0400

    fillin' gaps

--- SOEN487Project1/web/map.js
+++ SOEN487Project1/web/map.js
@@ -9,32 +9,6 @@ function initMap() {
     var $defaultLocation = "Montreal, QC";
     var $defaultTerm = "dinner";
 
-    /*$.ajax({
-        method: "GET",
-        url: "http://localhost:8080/SOEN487Project1/webresources/yelp",
-        dataType: "json",
-        success: function (businesses) {
-            $businessList.append('<h1><u>Results found for "' + $defaultTerm + '" in ' + $defaultLocation + '</u></h1>');
-
-            $.each(businesses, function (index, business) {
-                marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(business.location.coordinate.latitude,
-                            business.location.coordinate.longitude),
-                    map: map,
-                    title: business.name
-                });
-
-                $businessList.append("<h3>" + business.name + "</h3>" +
-                        business.location.display_address + "<br>");
-                
-                markers.push(marker);
-            });
-        },
-        error: function () {
-            alert("Error initializing map");
-        }
-    });*/
-
     $businessList.append("</div>");
     $('#business_div').append($businessList);
 }
