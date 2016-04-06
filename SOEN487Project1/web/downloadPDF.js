9cd2c3d6e9c1e559041c8e34e3b393b9c98ef557
Felicia Santoro-Petti <fsantoropetti@gmail.com>
2016-04-06 18:59:13 -0400

    Removed alerts

--- SOEN487Project1/web/downloadPDF.js
+++ SOEN487Project1/web/downloadPDF.js
@@ -10,10 +10,7 @@ $(function () {
         
         var divToPrint = $('#itinerary').get(0);
         var htmlToPrint = $(divToPrint).clone();
-        alert($(divToPrint).html());
-        alert($(htmlToPrint).html());
         $(htmlToPrint).find('.remove').remove();
-        alert($(htmlToPrint).html());
         doc.fromHTML($(htmlToPrint).html(), 15, 15, {
             'width': 170,
             'elementHandlers': specialElementHandlers
